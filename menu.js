f7f405f418ad5b8ec29d1ea603b1748c7fed4e42
devLarissaRodrigues <larissa.rodrigues@ccc.ufcg.edu.br>
2024-06-25 09:02:54 -0300

    Adding links

--- menu.js
+++ menu.js
@@ -13,3 +13,16 @@ menu.addEventListener('click', ()=>{
 overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
 })
+
+
+// scripts.js
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
