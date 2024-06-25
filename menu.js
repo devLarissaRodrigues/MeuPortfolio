184cd817b9df4b64c7eb3c4b584f6405f061f372
devLarissaRodrigues <larissa.rodrigues@ccc.ufcg.edu.br>
2024-06-25 09:54:07 -0300

    Fixing responsive menu

--- menu.js
+++ menu.js
@@ -1,47 +1,17 @@
-let botaoMenu = document.getElementById('botao-menu')
-let menu = document.getElementById('menu-mobile')
-let overlay = document.getElementById('overlay-menu')
+let botaoMenu = document.getElementById('botao-menu');
+let menuMobile = document.getElementById('menu-mobile');
+let overlay = document.getElementById('overlay-menu');
 
-botaoMenu.addEventListener('click', ()=>{
-    menu.classList.add('abrir-menu')
+botaoMenu.addEventListener('click', ()=> {
+    menuMobile.classList.toggle('abrir-menu');
 })
 
-menu.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu')
+let botaoFechar = document.querySelector('.botao-fechar');
+
+botaoFechar.addEventListener('click', () => {
+    menuMobile.classList.remove('abrir-menu');
 })
 
 overlay.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu')
+    menuMobile.classList.remove('abrir-menu');
 })
-
-
-// scripts.js
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
-
-// Mostrar ou esconder o botão ao rolar a página
-window.addEventListener('scroll', () => {
-    const scrollToTopBtn = document.getElementById('scrollToTopBtn');
-    if (window.pageYOffset > 300) { // Exibir o botão após rolar 300px
-        scrollToTopBtn.classList.add('show');
-    } else {
-        scrollToTopBtn.classList.remove('show');
-    }
-});
-
-// Adicionar a funcionalidade de rolar ao topo
-document.getElementById('scrollToTopBtn').addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-});
